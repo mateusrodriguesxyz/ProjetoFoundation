91254f16d62980c895f531aafec7e16be17dc5f0
Mateus Rodrigues <mateus.fr26@gmail.com>
2022-11-03 14:36:08 -0300

    Initial Commit

---
+++ ProjetoFoundation/ProjetoFoundationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjetoFoundationApp.swift
+//  ProjetoFoundation
+//
+//  Created by Mateus on 03/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ProjetoFoundationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
