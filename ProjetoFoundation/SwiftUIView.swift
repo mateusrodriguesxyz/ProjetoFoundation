eb61053b1dfcd1de7f00e62438aba32959e5d19f
Mateus Rodrigues <mateus.fr26@gmail.com>
2022-11-03 15:13:49 -0300

    Add new file

---
+++ ProjetoFoundation/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  ProjetoFoundation
+//
+//  Created by Mateus on 03/11/22.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
